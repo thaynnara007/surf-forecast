5927bc94cffcae5a818a6aa66b4b1d82001bb8dd
thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2020-11-05 14:19:29 -0300

    add forecast client

---
+++ src/clients/__test__/stormGlass.test.ts
@@ -0,0 +1,21 @@
+import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+import stormGlassWether3HoursFixture from '@test/fixtures/stormglass_wether_3_hours.json';
+import { StormGlass } from '@src/clients/stormGlass';
+import axios from 'axios';
+
+jest.mock('axios');
+
+describe('StromGlass client', () => {
+  it('Should return the normalized forecast from the StromGlass service', async () => {
+    const lat = -33.792726;
+    const lng = 151.289824;
+
+    axios.get = jest.fn().mockResolvedValue({ data: stormGlassWether3HoursFixture });
+
+    const stormGlass = new StormGlass(axios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+
+    expect(response).toEqual(stormGlassNormalized3HoursFixture);
+  });
+});
+
--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -45,4 +45,3 @@ describe('Beach forecast functional tests', () => {
     ]);
   });
 });
-
